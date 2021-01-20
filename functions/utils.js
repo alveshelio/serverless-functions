4286669f3366d2c4fdc50103d288b8d04ecb7667
Helio <helios25@gmail.com>
2021-01-20 16:48:05 +0000

    :fire: removed comments

--- functions/utils.js
+++ functions/utils.js
@@ -1,8 +1,5 @@
 const { GraphQLClient } = require('graphql-request')
 
-console.warn('HASURLA_GQL_API', process.env.HASURLA_GQL_API)
-console.warn('HASURLA_GQL_ADMIN_SECRET', process.env.HASURA_GQL_ADMIN_SECRET)
-
 const client = new GraphQLClient(process.env.HASURLA_GQL_API, {
   headers: {
     ['x-hasura-admin-secret']: process.env.HASURA_GQL_ADMIN_SECRET,
